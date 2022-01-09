0eaedcd2db1db7379ec5c18eaad0770c87e4f8ec
Thiago <thiago.ralves02@gmail.com>
2022-01-09 13:00:48 -0300

    fix: download cv avaible

--- src/components/Introducao/Introducao.js
+++ src/components/Introducao/Introducao.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import './Introducao.css'
 import homeImg from '../../assets/images/home-image.svg'
-
+import curriculo from '../../assets/images/curriculo.pdf'
 export default function Introducao() {
     return (
         <div className="container-home">
             <div>
                 <h2 >Olá, eu sou o <br/><h2 className='line type-animation'> Thiago Roberto :)</h2></h2>
                 <h3>Desenvolvedor Fullstack</h3>
-                <a href='./static/images/curriculo.pdf'download='curriculo.pdf' target='_blank'><button className="btn-download">Download CV</button></a>
+                <a href={curriculo} download='curriculo.pdf' target='_blank'><button className="btn-download">Download CV</button></a>
                 <a href='#container-contato'><button className='btn-contato'>Entrar em contato</button></a>
             </div>
             <div>
